64e764ebdca9ec7c2a722ce913f8b3a017e88586
tireshii <egor.trusakov@gmail.com>
2022-08-07 21:47:23 +0300

    minor styles for packs

--- src/elements/packicon.jsx
+++ src/elements/packicon.jsx
@@ -2,9 +2,9 @@ const Pack = ({props}) => {
     console.log(props)
     //no action yet
     return (
-        <div className="">  
-            {props.name}
-            {props.price}
+        <div className="pack-icon">  
+            <h3>{props.name}</h3>
+            <h2>Price: {props.price}</h2>
         </div>
     )
 }
