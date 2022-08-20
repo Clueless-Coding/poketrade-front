600517248da2faba2d7df7971e2b9b4dd0e34d11
tireshii <egor.trusakov@gmail.com>
2022-08-20 19:58:17 +0300

    searching pack input

--- src/pages/packs.jsx
+++ src/pages/packs.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import Pack from "./elements/packicon";
 const Packs = ({packs, user}) => {
     const Packlist= () => (packs.map((item, i) => <Pack props={item} key={i}/>))
+    const [filtered, setFiltered] = useState([])
+    const handleFilter = (props) => {
+        setFiltered(packs.filter(item => item.name.toLowerCase().includes(props.toLowerCase())).map((item, i) => <Pack props={item} key={i}/>))
+    }
     return(
         <>
+            <h4>Find specific pack</h4>
+            <input 
+                placeholder="Pack name"
+                className="input"
+                onChange={({target}) => handleFilter(target.value)}
+            />
             <div>
                 <h4>Your balance: {user.balance}</h4>
             </div>
             <div className="pack-page-content">
-                <Packlist/>
+                {filtered.length > 0 ? filtered : <Packlist/>}
             </div>
         </>
     )
